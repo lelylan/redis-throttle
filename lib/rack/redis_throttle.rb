4e66aa8956c0db0c80df5e175612cd6ccfdc1f40
Anthony Scalisi <scalisi.a@gmail.com>
2014-03-03 23:10:02 -0800

    Update redis_throttle.rb
    Fix typo in autoload

--- lib/rack/redis_throttle.rb
+++ lib/rack/redis_throttle.rb
@@ -13,7 +13,7 @@ module Rack
     autoload :Limiter,    'rack/redis_throttle/limiter'
     autoload :TimeWindow, 'rack/redis_throttle/time_window'
     autoload :Daily,      'rack/redis_throttle/daily'
-    autoload :Intrval,    'rack/redis_throttle/interval'
+    autoload :Interval,   'rack/redis_throttle/interval'
     autoload :VERSION,    'rack/redis_throttle/version'
   end
 end
